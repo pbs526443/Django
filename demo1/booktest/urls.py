6d897db9914cb7ba112d5d71ce6ffc402b5a48ce
pbs <1768804958@qq.com>
2019-04-17 16:03:00 +0800

    使用模板

--- demo1/booktest/urls.py
+++ demo1/booktest/urls.py
@@ -3,7 +3,8 @@ from . import views
 urlpatterns = [
     # url('index/',views.index),
     url(r'list/',views.list),
-    url(r'index/$',views.index),
+    # url(r'index/$',views.index),
     url(r'detail/(\d+)/(\d+)/$',views.detail),
     url(r'details/(\d+)/$',views.details),
+    url(r'$', views.index),
 ]
--- demo1/booktest/views.py
+++ demo1/booktest/views.py
@@ -1,11 +1,19 @@
 from django.shortcuts import render
 from django.http import  HttpResponse
 from .models import BookInfo,HeroInfo
+from django.template import loader
 # Create your views here.
 
+# 定义视图函数
 def index(request):
-    print("请求",request)
-    return HttpResponse('首页')
+    # print("请求",request)
+    # return HttpResponse('首页')
+    indextem = loader.get_template('booktest/index.html')
+    cont = {"username":"pbs"}
+    # 使用变量参数渲染模板
+    result = indextem.render(cont)
+    # 返回模板
+    return HttpResponse(result)
 
 
 def list(request):
@@ -16,10 +24,24 @@ def detail(request,id,age):
     return HttpResponse('标签页'+'  '+str(id)+'  '+str(age))
 
 def details(request,id):
-    result = BookInfo.objects.get(pk=int(id)).btitle
-    return HttpResponse(result)
+    try:
+        result = BookInfo.objects.get(pk=int(id)).btitle
+        return HttpResponse(result)
+    except Exception as e:
+        print(e)
+
 
 '''
 视图函数
 将函数和路由绑定
+'''
+
+'''
+创建模板文件夹 templates
+配置模板目录 os.path.join(BASEDIR,'templates')
+创建项目模板目录，创建模板
+
+加载模板 loader.get_template()
+使用变量渲染模板  result = temp.render({})
+返回模板 HttpResponse(result)
 '''
