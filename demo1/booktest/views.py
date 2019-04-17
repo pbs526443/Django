05cca122260dfc081fc1da0614d26fa77b70eeb7
pbs <1768804958@qq.com>
2019-04-17 15:16:35 +0800

    url传递参数

--- demo1/booktest/views.py
+++ demo1/booktest/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import  HttpResponse
+from .models import BookInfo,HeroInfo
 # Create your views here.
 
 def index(request):
@@ -8,7 +9,16 @@ def index(request):
 
 
 def list(request):
+
     return HttpResponse('列表页')
+
+def detail(request,id,age):
+    return HttpResponse('标签页'+'  '+str(id)+'  '+str(age))
+
+def details(request,id):
+    result = BookInfo.objects.get(pk=int(id)).btitle
+    return HttpResponse(result)
+
 '''
 视图函数
 将函数和路由绑定
